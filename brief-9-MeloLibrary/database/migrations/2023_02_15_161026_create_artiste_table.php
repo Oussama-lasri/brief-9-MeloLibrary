bc4ed826b9cb3ca70ba6b10f7b899fb42c9a1ddc
ousama lasri <ousamalasri2@gmail.com>
2023-02-15 17:18:24 +0100

    creation de base de donnee

---
+++ brief-9-MeloLibrary/database/migrations/2023_02_15_161026_create_artiste_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artiste', function (Blueprint $table) {
+            $table->id('id_artiste');
+            $table->string('nom');
+            $table->date('date_naissqance');
+            $table->binary('image');
+            $table->foreign('id_band')->references('id_band')->on('bande')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artiste');
+    }
+};
---
+++ brief-9-MeloLibrary/database/migrations/2023_02_15_161706_create_bande_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bande', function (Blueprint $table) {
+            $table->id('id_bande');
+            $table->string('image');
+            $table->id('date_creation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bande');
+    }
+};
